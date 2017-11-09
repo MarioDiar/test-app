cad02d28a03532170c69e2c078bf7277276ec825
Mario Diaz <mariodiar@gmail.com>
2017-11-09 17:41:36 -0600

    Adding more routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
   def home
-    CounterJob.perform_later
     @visits = Count.first().nil? ? 0 : Count.first().count
+  end
+
+  def count
+    CounterJob.perform_later
+    redirect_to root_path
+  end
+
+  def status
+    render plain: "Ok"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/home'
+
+  get '/count',     to: 'pages#count',   as: 'count'
+  get '/status',    to: 'pages#status', as: 'status'
+
   root 'pages#home'
 end
